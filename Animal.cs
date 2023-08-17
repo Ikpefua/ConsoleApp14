25750c7849f0318788c5864ab3e489ccb1512bc8
IkpefuaTosin <tosaino34@outlook.com>
2023-08-17 12:36:43 +0100

    Add project files.

---
+++ Animal.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+    //Unlike Java, C# allows you to write multiple classes in a single source file.
+
+    //Here is a tutorial example C# source file that contains 2 classes, UtilTest.cs:
+   public class Utili 
+    { 
+       public static void Swap(ref string y,  ref string x)
+        {
+            string o = x;
+            x = y;
+            y = o;
+        }     
+    }
+
+
+    
+    public class Program1
+    {   
+        public static void Swap(ref string w, ref string t)
+        {
+            string a = t;
+            t = w;
+            w = a;
+        }
+    }
+    
+
+
+    //Abstarct class
+    internal abstract class AnimalS
+    {
+        //Abstarct method(Dont have a body)
+        public abstract void animalSleepSound();
+        //Regular Method
+        public  void playSleepSound() 
+        {
+            Console.WriteLine("\"Zzmmzzm\"");
+        }
+    }
+    // Derived class(from Animal)
+    class Pig : AnimalS 
+    {
+        public override void animalSleepSound()
+        {
+            Console.WriteLine("The pig sleep and say: ");
+        }
+    }
+
+    
+
+    abstract class Car //Abstract Base Class (Parent).
+    {
+        //Abstract method dont have a body
+        public abstract void CarOn();
+        // Regular Method
+        public void CarSound() 
+        {
+            Console.WriteLine("\"Voooooom Voooooooom!!!\" ");
+        }
+    }
+    class BrandSound : Car //Derived class(Child)
+    {
+        public override void CarOn()
+        {
+            var carBrand = "VolksWageen ";
+            Console.WriteLine(carBrand + "" + "Sound When kick start is: ");
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,91 @@
+﻿using ConsoleApp14;
+using System;
+
+namespace Polymo
+{
+    class Animal // Base class (Parent)
+    {
+        public virtual void animalSound()
+        {
+            Console.WriteLine("The Following Animals Makes These Sounds ");
+        }
+    }
+
+    class Dog : Animal //Derived class (Child)
+    {
+        public override void animalSound()
+        {
+            Console.WriteLine("The Dog says: \"bow bow bow!!.\" ");
+        }
+    }
+    class Cat : Animal //Derived class (Child)
+    {
+        public override void animalSound()
+        {
+            Console.WriteLine("The Cat says: \"Meow\" \"Meow!!.\" ");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "Blue";
+            string b = "Sky is";
+            Console.WriteLine("{0} {1} ", a, b);
+            Utili.Swap(ref a, ref b);
+            Console.WriteLine("{0} {1} ", a, b);
+
+            string i = "My Name Is";
+            string q = "Tosin,";
+            Console.WriteLine("---------");
+            Console.WriteLine("{0} {1}", i, q);
+            Program1.Swap(ref i, ref q);
+            Console.WriteLine("{0} {1}", i, q);
+            Console.WriteLine("---------");
+
+
+            Animal animal = new Animal();
+            Animal dog = new Dog();
+            Animal cat = new Cat();
+           
+            animal.animalSound();
+            Console.WriteLine("-----------------------");
+            
+            dog.animalSound();
+            cat.animalSound();
+            Console.WriteLine("-----------------------");
+            
+
+            //Dog dog = new Dog();
+            //Cat cow = new Cat();
+            //animal.animalSound();
+            //dog.animalSound();
+            //cow.animalSound();
+
+            Pig pig = new Pig();
+            Console.WriteLine("-----------");
+            pig.animalSleepSound();
+            Console.WriteLine("");
+            pig.playSleepSound();
+
+
+            BrandSound myCarObj = new BrandSound();
+            Console.WriteLine("-----------");  
+            
+            myCarObj.CarOn();
+            myCarObj.CarSound();
+           
+            Console.WriteLine("-----------");
+            Console.Write("Enter Your Mother Age: = ");
+            double motherAge = double.Parse(Console.ReadLine());
+            Console.WriteLine("--------");
+            Console.WriteLine();
+            Console.WriteLine("My Mum's Age is: {0} ", motherAge);
+
+
+        }
+
+    }
+
+}
